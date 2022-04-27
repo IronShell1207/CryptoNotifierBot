986bdf870d7be0d5a288dd8b642ba8759c2ad061
Vitali <unknown.objc@gmail.com>
2022-04-27 13:05:08 +0300

    Added bitget spot link, and fixed pair getter

--- CryptoNotifierBot/Constants/ExchangesSpotLinks.cs
+++ CryptoNotifierBot/Constants/ExchangesSpotLinks.cs
@@ -12,19 +12,20 @@ namespace CryptoApi.Constants
         /// <summary>
         /// Okx spot page, pair pattern base-quote
         /// </summary>
-        public const string OkxSpot = "https://www.okx.com/ru/trade-spot/";
+        public const string OkxSpot = "https://www.okx.com/ru/trade-spot/{0}";
         /// <summary>
         /// Binance spot page, pair pattern BASE_QUOTE
         /// </summary>
-        public const string BinanceSpot = "https://www.binance.com/ru/trade/";
+        public const string BinanceSpot = "https://www.binance.com/ru/trade/{0}";
         /// <summary>
         /// GateIO spot page, pair pattern BASE_QUOTE
         /// </summary>
-        public const string GateIoSpot = "https://www.gate.io/tradepro/";
+        public const string GateIoSpot = "https://www.gate.io/tradepro/{0}";
         /// <summary>
         /// GateIO spot page, pair pattern BASE-QUOTE
         /// </summary>
-        public const string KucoinSpot = "https://www.kucoin.com/ru/trade/";
+        public const string KucoinSpot = "https://www.kucoin.com/ru/trade/{0}";
+        public const string BitGetSpot = "https://www.bitget.com/en/spot/{0}_SPBL";
 
         public static string GetSpotLink(string exchange)
         {
@@ -34,7 +35,15 @@ namespace CryptoApi.Constants
             else if (exchange == Exchanges.Okx) return OkxSpot;
             else if (exchange == Exchanges.GateIO) return GateIoSpot;
             else if (exchange == Exchanges.Kucoin) return KucoinSpot;
+            else if (exchange == Exchanges.Bitget) return BitGetSpot;
             return null;
+        }
+
+        public static string GetPairLink(string exchange, string Base, string Quote)
+        {
+            var spotlink = GetSpotLink(exchange);
+            var pairConverted = GetPairConverted(exchange, Base, Quote);
+            return string.Format(spotlink, pairConverted);
         }
         public static string GetPairConverted(string exchange, string Base, string Quote)
         {
@@ -44,7 +53,7 @@ namespace CryptoApi.Constants
             else if (exchange == Exchanges.Okx)    return $"{Base}-{Quote}";
             else if (exchange == Exchanges.GateIO) return $"{Base}-{Quote}";
             else if (exchange == Exchanges.Kucoin) return $"{Base}_{Quote}";
-            else if (exchange == Exchanges.Bitget) return $"{Base}_{Quote}";
+            else if (exchange == Exchanges.Bitget) return $"{Base}{Quote}";
             return null;
         }
     }
