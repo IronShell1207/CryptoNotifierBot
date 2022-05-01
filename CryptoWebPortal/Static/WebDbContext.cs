04869b13190ad4a8402ab305c1b0fd506116800f
Vitali <unknown.objc@gmail.com>
2022-05-01 18:59:26 +0300

    web created

---
+++ CryptoWebPortal/Static/WebDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CryptoWebPortal.Objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoWebPortal.Static
+{
+    public class WebDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"/Tcryptobot/";
+            if (!Directory.Exists(dbPath)) Directory.CreateDirectory(dbPath);
+            dbPath = $"Filename={dbPath}webdb.db";
+            optionsBuilder.UseSqlite(dbPath);
+        }
+
+        public WebDbContext()
+        {
+            var migration = Database.GetPendingMigrations();
+            if (migration.Any())
+                Database.Migrate();
+        }
+
+    }
+}
