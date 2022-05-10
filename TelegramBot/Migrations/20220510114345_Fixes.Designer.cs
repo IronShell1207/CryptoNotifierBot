89b2302b2be0ac75b6f17629561f828c2b4af600
Vitali <unknown.objc@gmail.com>
2022-05-10 16:17:20 +0300

    Fixes

--- TelegramBot/Migrations/20220510114345_Fixes.Designer.cs
+++ TelegramBot/Migrations/20220510114345_Fixes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TelegramBot.Static;
 
@@ -10,9 +11,10 @@ using TelegramBot.Static;
 namespace TelegramBot.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220510114345_Fixes")]
+    partial class Fixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.4");
--- TelegramBot/Static/BotLoops/MonitorLoop.cs
+++ TelegramBot/Static/BotLoops/MonitorLoop.cs
@@ -25,20 +25,34 @@ namespace TelegramBot.Static.BotLoops
                     foreach (UserConfig user in users)
                     {
                         StringBuilder sb = new StringBuilder();
-                        List<(CryptoPair, double)> pairsDefault = await UserTasksToNotify(user, dbContext, true);
-                        List<(CryptoPair, double)> pairsSingleNotify;
+                        var pairsDefault = await UserTasksToNotify(user, dbContext, true);
+                        var pairsSingleNotify = await UserTasksSingleNotify(user, dbContext);
+                        var pairsTriggeredButRaised = await UserTriggeredTasksRaised(user, dbContext);
                         if (pairsDefault.Any())
                         {
                             foreach (var pair in pairsDefault)
                                 sb.AppendLine(FormatNotifyEntryStock(pair.Item1, pair.Item2));
-
                             lastUpdateUsers.Add(new IntervaledUsersHistory(user.Id, DateTime.Now));
                             await BotApi.SendMessage(user.TelegramId, sb.ToString());
                         }
+
+                        if (pairsSingleNotify.Any())
+                        {
+                            foreach (var pair in pairsSingleNotify)
+                                sb.AppendLine(FormatNotifyEntryStock(pair.Item1, pair.Item2));
+                            await BotApi.SendMessage(user.TelegramId, sb.ToString());
+                        }
+
+                        if (pairsTriggeredButRaised.Any())
+                        {
+                            foreach (var pair in pairsTriggeredButRaised)
+                                sb.AppendLine(FormatNotifyEntryStock(pair.Item1, pair.Item2));
+                            await BotApi.SendMessage(user.TelegramId, $"⚠️Pairs triggered, but raise above or fall bellow trigger again:\n{sb.ToString()}");
+                        }
                     }
                 }
+                Thread.Sleep(420);
             }
-            Thread.Sleep(1420);
         }
 
         public static async Task<List<(CryptoPair, double)>> UserTasksToNotify(UserConfig user, AppDbContext dbContext, bool useInterval = true)
@@ -53,7 +67,8 @@ namespace TelegramBot.Static.BotLoops
                 foreach (var pair in pairs)
                 {
                     var price = await Program.cryptoData.GetCurrentPricePairByName(pair.ToTradingPair());
-                    if (price.Price > 0 &&( price.Price > pair.Price && pair.GainOrFall || price.Price < pair.Price && !pair.GainOrFall))
+                    if (price.Price > 0 &&(price.Price > pair.Price && pair.GainOrFall || 
+                        price.Price < pair.Price && !pair.GainOrFall))
                         tasks.Add(new(pair, price.Price));
                 }
             }
@@ -65,17 +80,44 @@ namespace TelegramBot.Static.BotLoops
         {
             List<(CryptoPair, double)> tasksReturing = new List<(CryptoPair, double)>();
             var pairsSingle = user?.pairs?.Where(x => x.TriggerOnce && !x.Triggered)?.ToList();
-            var pairsTriggered = user?.pairs?.Where(x => x.TriggerOnce && x.Triggered)?.ToList();
+           
             if (pairsSingle?.Count > 0)
             {
                 foreach (var pair in pairsSingle)
                 {
                     var price = await Program.cryptoData.GetCurrentPricePairByName(pair.ToTradingPair());
-                    if (price.Price > 0 && (price.Price > pair.Price && pair.GainOrFall || price.Price < pair.Price && !pair.GainOrFall))
+                    if (price.Price > 0 && (price.Price > pair.Price && pair.GainOrFall ||
+                                            price.Price < pair.Price && !pair.GainOrFall))
+                    {
                         tasksReturing.Add(new(pair, price.Price));
+                        pair.Triggered = true;
+                    }
                 }
+
+                dbContext.SaveChangesAsync();
             }
 
+            return tasksReturing;
+        }
+
+        public static async Task<List<(CryptoPair, double)>> UserTriggeredTasksRaised(UserConfig user, AppDbContext dbContext)
+        {
+            List<(CryptoPair, double)> tasksReturing = new List<(CryptoPair, double)>();
+            var pairsTriggered = user?.pairs?.Where(x => x.TriggerOnce && x.Triggered)?.ToList();
+            if (pairsTriggered?.Count > 0)
+            {
+                foreach (var pair in pairsTriggered)
+                {
+                    var price = await Program.cryptoData.GetCurrentPricePairByName(pair.ToTradingPair());
+                    if (price.Price > 0 && ((price.Price * 1.01) < pair.Price && pair.GainOrFall ||
+                                            (price.Price * 0.99) > pair.Price && !pair.GainOrFall))
+                    {
+                        tasksReturing.Add(new(pair, price.Price));
+                        pair.Triggered = false;
+                    }
+                }
+                dbContext.SaveChangesAsync();
+            }
             return tasksReturing;
         }
 
