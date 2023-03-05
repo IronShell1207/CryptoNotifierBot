615da7892f85f8df0eaea97e46bb9b0e335659de
nev <nev@DESKTOP-MN0QHGG>
2023-03-05 22:27:16 +0700

    Add basic view models

--- CryptoMonWidgets/View/Pages/MainPage.xaml.cs
+++ CryptoMonWidgets/View/Pages/MainPage.xaml.cs
@@ -1,20 +1,10 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using Microsoft.UI.Xaml;
+using CryptoMonWidgets.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,6 +19,11 @@ namespace CryptoMonWidgets.View.Pages
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DataContext ??= App.ServiceProvider.GetRequiredService<MainViewModel>();
         }
     }
 }
---
+++ CryptoMonWidgets/ViewModels/Components/PairViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CryptoMonWidgets.ViewModels.Components
+{
+    public class PairViewModel : ObservableObject
+    {
+        private bool _lastChangeSide;
+        private string _pairBaseName;
+
+        private string _pairQuoteName;
+
+        private double _price;
+
+        private double _priceChange;
+
+        private double _priceChangePercent;
+
+        public bool LastChangeSide
+        {
+            get => _lastChangeSide;
+            set => SetProperty(ref _lastChangeSide, value);
+        }
+
+        public string PairBaseName
+        {
+            get => _pairBaseName;
+            set => SetProperty(ref _pairBaseName, value);
+        }
+
+        public string PairQuoteName
+        {
+            get => _pairQuoteName;
+            set => SetProperty(ref _pairQuoteName, value);
+        }
+
+        public double Price
+        {
+            get => _price;
+            set => SetProperty(ref _price, value);
+        }
+
+        public double PriceChange
+        {
+            get => _priceChange;
+            set => SetProperty(ref _priceChange, value);
+        }
+
+        public double PriceChangePercent
+        {
+            get => _priceChangePercent;
+            set => SetProperty(ref _priceChangePercent, value);
+        }
+    }
+}
